b97ae08d3310faa8fbe7f400a0391c453d069642
SaifullahUsmani693 <amjadmehmoodusmani693@gmail.com>
2023-11-02 19:25:22 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,45 +1,34 @@
 // Using load method to wait till the page is loaded so we don't face didn't found an element type of error
-window.addEventListener('load', 
-function() {
-	// Checking if watch is in the url so we can block that page
-	if(window.location.href.includes("watch")){
-		disable_Page();
-		console.log("Found Video Content On Facebook");
-		console.log("Disabling Page");
-		
+window.addEventListener('load',
+	function () {
+		// Checking if watch is in the url so we can block that page
+		if (window.location.href.includes("facebook") && window.location.href.includes("watch")) {
+			disable_Page();
 
-	}else{
-		console.log("No Video Content Found");
-		console.log("No Action Required");
+		} else if (window.location.href.includes("shorts")) {
+			disable_Page();
 
-		document.body.addEventListener('click', function(){
-				console.log("Got a click in body");
-				setTimeout(function () {
-					console.log("5 sec passed by..");
-					disable_Page(); 
-				}, 5000);
-			}, true);
 		}
-	}, 	
-false);
+	},
+	false);
 
 
 function disable_Page() {
 	var should_disable_page;
 	// using a bool to see if watch keyword is in the url if yes then disable the page else not
-	if(window.location.href.includes("watch")){
+	if (window.location.href.includes("watch")) {
 		should_disable_page = true;
-	}else{
-		should_disable_page = false;		
+	} else {
+		should_disable_page = false;
 	}
 
-		
-	if(should_disable_page){
+
+	if (should_disable_page) {
 		// Deleting head and body so the content cannot be recovered using developer tools
-		var x = document.getElementsByTagName("head")[0];   
+		var x = document.getElementsByTagName("head")[0];
 		x.remove();
 
-		var y = document.getElementsByTagName("body")[0];   
+		var y = document.getElementsByTagName("body")[0];
 		y.remove();
 
 		//Creating the body and message element using the createElement method.
