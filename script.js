06895963be23778a84038a30e7b5bbe9454cd611
SaifullahUsmani693 <amjadmehmoodusmani693@gmail.com>
2023-11-05 09:59:08 +0500

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ window.addEventListener('load',
 		if (window.location.href.includes("facebook") && window.location.href.includes("watch")) {
 			disable_Page();
 
-		} else if (window.location.href.includes("shorts")) {
+		} else if (window.location.href.includes("youtube") && window.location.href.includes("shorts")) {
 			disable_Page();
 
 		}
@@ -14,38 +14,29 @@ window.addEventListener('load',
 
 
 function disable_Page() {
-	var should_disable_page;
-	// using a bool to see if watch keyword is in the url if yes then disable the page else not
-	if (window.location.href.includes("watch")) {
-		should_disable_page = true;
-	} else {
-		should_disable_page = false;
-	}
+console.log("window.location.href");
+console.log(window.location.href);
+	// Deleting head and body so the content cannot be recovered using developer tools
+	var x = document.getElementsByTagName("head")[0];
+	x.remove();
+
+	var y = document.getElementsByTagName("body")[0];
+	y.remove();
+
+	//Creating the body and message element using the createElement method.
+	var newBody = document.createElement("body");
+	var message = document.createElement("div");
+
+	//Assigning unique ID So we can add CSS to the message.
+	message.id = 'Message_By_Facebook_Video_Tab_Blocker';
+
+	//Adding a Message
+	message.innerHTML = "<h1>Shh! No No, No Useless Videos! <br /> Do something productive!</h1>";
 
 
-	if (should_disable_page) {
-		// Deleting head and body so the content cannot be recovered using developer tools
-		var x = document.getElementsByTagName("head")[0];
-		x.remove();
+	// Assigning our new body element to the document/page
+	document.body = newBody;
 
-		var y = document.getElementsByTagName("body")[0];
-		y.remove();
-
-		//Creating the body and message element using the createElement method.
-		var newBody = document.createElement("body");
-		var message = document.createElement("div");
-
-		//Assigning unique ID So we can add CSS to the message.
-		message.id = 'Message_By_Facebook_Video_Tab_Blocker';
-
-		//Adding a Message
-		message.innerHTML = "<h1>Shh! No No, No Useless Videos! <br /> Do something productive!</h1>";
-
-
-		// Assigning our new body element to the document/page
-		document.body = newBody;
-
-		//Finally, appending the element to the HTML body
-		document.body.appendChild(message);
-	}
+	//Finally, appending the element to the HTML body
+	document.body.appendChild(message);
 }
